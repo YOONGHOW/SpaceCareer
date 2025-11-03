78f9231c13399e37cc256eb3d318ab6515bc8be9
YOONGHOW <yoonghow123@gmail.com>
2025-11-03 16:57:52 +0800

    push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,7 @@ export default function RootLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="(employerTabs)" />
-
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(job-seekerTabs)" />
     </Stack>
   );
 }
